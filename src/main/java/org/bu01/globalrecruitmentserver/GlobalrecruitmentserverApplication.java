9a050b0cf939ec6582db154bf056247da86fe2e6
Lawliet529 <85656044+Lawliet529@users.noreply.github.com>
2023-07-06 14:45:01 +0700

    Initial commit

---
+++ src/main/java/org/bu01/globalrecruitmentserver/GlobalrecruitmentserverApplication.java
@@ -0,0 +1,13 @@
+package org.bu01.globalrecruitmentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalrecruitmentserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalrecruitmentserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bu01/globalrecruitmentserver/GlobalrecruitmentserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bu01.globalrecruitmentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalrecruitmentserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
